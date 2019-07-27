577bd2965d6b9bc6ffb558adb5d58c2ce5650e4f
Samuel M Smith <smith.samuel.m@gmail.com>
2019-07-27 14:54:07 -0600

    fixed

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    python_requires='>=3.7,
+    python_requires='>=3.7',
     install_requires=[
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
